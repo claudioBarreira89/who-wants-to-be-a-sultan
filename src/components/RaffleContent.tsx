ce6c751729acc6d1ec671e4bc2606c427aa96698
gedithejedi <gediminaspetreikis@yahoo.com>
2023-11-18 12:01:09 +0300

    feat: basic design done

--- src/components/RaffleContent.tsx
+++ src/components/RaffleContent.tsx
@@ -2,6 +2,25 @@ import { Button, Input } from "antd";
 import Image from "next/image";
 import { useState } from "react";
 
+const raffleData = [
+  {
+    title: "Total Winnings",
+    showcase: "800,000",
+  },
+  {
+    title: "Sultans made",
+    showcase: "8",
+  },
+  {
+    title: "Trees Planted",
+    showcase: "8,000",
+  },
+  {
+    title: "Average winnings",
+    showcase: "280,000",
+  },
+]
+
 const RaffleContent = () => {
   const [number, setNumber] = useState(0);
   const min = 1;
@@ -38,50 +57,66 @@ const RaffleContent = () => {
   }
 
   return (
-    <div className="flex justify-center items-center flex-col gap-4 rounded-3xl p-8 bg-cardBg">
-      <div className="relative rounded-full p-8 w-[300px] h-[300px] flex flex-col justify-center progress-bar" style={progressBarStyle}>
-        {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"> */}
-        <div className="relative z-10 flex justify-center">
-          <Image
-            src="/treasure-chest.svg"
-            height={30}
-            width={80}
-            alt="treasure chest"
-            className="w-[180px] h-auto"
-          />
-        </div>
-        <div className="relative z-10">
-          <p className="text-2xl mt-4 text-center">$400,000</p>
-        </div>
-      </div>
-
-      <div>
-        <div className="flex flex-col justify-center gap-4 mt-4">
-          <div className="flex justify-center gap-2">
-            <Button
-              className="disabled:bg-gray-100"
-              onClick={handleDecrease}
-              disabled={number <= min}
-            >-</Button>
-            <Input
-              min={min}
-              max={max}
-              className="w-8"
-              value={number}
-              onChange={handleChange}
+    <>
+      <div className="flex justify-center items-center flex-col gap-4 rounded-3xl p-8 bg-cardBg">
+        <div className="relative rounded-full p-8 w-[300px] h-[300px] flex flex-col justify-center progress-bar" style={progressBarStyle}>
+          <div className="relative z-10 flex justify-center">
+            <Image
+              src="/gold_chest.svg"
+              height={30}
+              width={80}
+              alt="treasure chest"
+              className="w-[180px] h-auto"
             />
-            <Button
-              onClick={handleIncrease}
-              disabled={number >= max}
-              className="disabled:bg-gray-100"
-            >+</Button>
           </div>
-          <p className="text-lg mb-4">1 ticket to the raffle costs ~$6. You may purchase up to 3 tickets.</p>
+          <div className="relative z-10">
+            <p className="text-2xl mt-4 text-center">$400,000</p>
+          </div>
+        </div>
 
-          <Button onClick={onRaffleJoin}>Buy raffle tickets</Button>
+        <div>
+          <div className="flex flex-col justify-center gap-4 mt-4">
+            <div className="flex justify-center gap-2">
+              <Button
+                className="disabled:bg-gray-100"
+                onClick={handleDecrease}
+                disabled={number <= min}
+              >-</Button>
+              <Input
+                min={min}
+                max={max}
+                className="w-8"
+                value={number}
+                onChange={handleChange}
+              />
+              <Button
+                onClick={handleIncrease}
+                disabled={number >= max}
+                className="disabled:bg-gray-100"
+              >+</Button>
+            </div>
+
+
+            <div className="w-full">
+              <Button className="w-full mb-2" onClick={onRaffleJoin}>Buy raffle tickets</Button>
+              <p className="text-xs p-0 max-w-[518px]">Thank You for you generosity. By purchasing this ticket you are directly supporting UNICEF's fight on climate change.</p>
+              <p className="text-xs p-0 max-w-[518px]">1 ticket to the raffle costs ~$6. You may purchase up to 3 tickets.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="px-8 mb-8">
+        <div className="flex items-center p-8 rounded-3xl bg-cardBg w-full mt-8">
+          {raffleData.map((raffle, index) => (
+            <div className={`pl-8 ${index == raffleData.length - 1 || "border-0 border-r border-white pr-8"}`}>
+              <p className="text-sm text-slate-300">{raffle.title}</p>
+              <p className="text-2xl">{raffle.showcase}</p>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
